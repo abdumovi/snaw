34a680711ed4ca8fb4d03d5e142f3189cd8a999e
abdumovi <abdumovi@gmail.com>
2021-09-15 20:26:32 +0500

    bags-changed

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let elbtn = document.querySelector('.language-list');
+let ellink = document.querySelector('.head-lenguage');
+
+let  add = function(){
+  elbtn.classList.add('open');
+ 
+}
+ellink.addEventListener('click',add);
+
+
+
+
+
+
+
